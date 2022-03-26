554699a219590a7ef50c0ff8aa76e0e272f7a5ec
Satabrata Paul <satabrata.slg88@gmail.com>
2022-03-27 01:01:37 +0530

    Virtual keyboard frame created

--- button.py
+++ button.py
@@ -2,13 +2,15 @@ import cv2
 
 
 class Button():
-    def __init__(self, pos, text, size=[100, 100]):
+    def __init__(self, pos, text, size=[85, 85]):
         self.pos = pos
         self.size = size
         self.text = text
 
     def draw_button(self, img):
-        cv2.rectangle(img, self.pos, (self.pos[0]+self.size[0], self.pos[1]+self.size[1]), (0, 0, 0), cv2.FILLED)
-        cv2.putText(img, self.text, (self.pos[0] + 25, self.pos[1] + 72), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255),
+        x, y = self.pos
+        w, h = self.size
+        cv2.rectangle(img, self.pos, (x + w, y + h), (0, 0, 0), cv2.FILLED)
+        cv2.putText(img, self.text, (x + 20, y + 65), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255),
                     5)
         return img
--- main.py
+++ main.py
@@ -7,17 +7,15 @@ capture.set(3, 1280)
 capture.set(4, 720)
 
 detector = HandDetector(detectionCon=0.8, maxHands=2)
-mybutton1 = Button([100, 100], 'Q')
-mybutton2 = Button([230, 100], 'W')
-mybutton3 = Button([360, 100], 'E')
-mybutton4 = Button([490, 100], 'R')
-mybutton5 = Button([620, 100], 'T')
-mybutton6 = Button([750, 100], 'Y')
-mybutton7 = Button([880, 100], 'U')
-mybutton8 = Button([1010, 100], 'I')
-mybutton9 = Button([100, 230], 'O')
-mybutton10 = Button([230, 230], 'P')
 
+button_list = []
+keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'"],
+        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
+
+for i in range(len(keys)):
+    for x, k in enumerate(keys[i]):
+        button_list.append(Button((100*x+50, 100*i+50), k))
 
 
 while True:
@@ -37,15 +35,8 @@ while True:
             lmList2 = hand2['lmList']
             bbox2 = hand2['bbox']
             fingers2 = detector.fingersUp(hand2)
-    img = mybutton1.draw_button(img)
-    img = mybutton2.draw_button(img)
-    img = mybutton3.draw_button(img)
-    img = mybutton4.draw_button(img)
-    img = mybutton5.draw_button(img)
-    img = mybutton6.draw_button(img)
-    img = mybutton7.draw_button(img)
-    img = mybutton8.draw_button(img)
-    img = mybutton9.draw_button(img)
-    img = mybutton10.draw_button(img)
+
+    for obj in button_list:
+        img = obj.draw_button(img)
     cv2.imshow("Myimage", img)
     cv2.waitKey(2)
